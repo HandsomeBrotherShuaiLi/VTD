b8710df0575e61ca6204689ec081ee53ec76b2ac
chris.li2 <chris.li2@nio.com>
2019-04-10 20:49:24 +0800

    add data generator plugins

--- Detection/AdvancedEAST/data_generator.py
+++ Detection/AdvancedEAST/data_generator.py
@@ -37,3 +37,40 @@ def gen(batch_size=cfg.batch_size, is_val=False):
                                    img_filename[:-4] + '_gt.npy')
             y[i] = np.load(gt_file)
         yield x, y
+
+def gen_plus(batch_size=cfg.batch_size, is_val=False):
+    img_h, img_w = cfg.max_train_img_size, cfg.max_train_img_size
+    x = np.zeros((batch_size, img_h, img_w, cfg.num_channels), dtype=np.float32)
+    pixel_num_h = img_h // cfg.pixel_size
+    pixel_num_w = img_w // cfg.pixel_size
+    y = np.zeros((batch_size, pixel_num_h, pixel_num_w, 7), dtype=np.float32)
+    if is_val:
+        with open(os.path.join(cfg.data_dir, cfg.val_fname), 'r') as f_val:
+            f_list = f_val.readlines()
+
+    else:
+        with open(os.path.join(cfg.data_dir, cfg.train_fname), 'r') as f_train:
+            f_list = f_train.readlines()
+
+    while True:
+        count=0
+        for i in range(len(f_list)):
+            one_img=f_list[i]
+            img_filename=one_img.strip('\n').split(',')[0]
+            img_path = os.path.join(cfg.data_dir,
+                                    cfg.train_image_dir_name,
+                                    img_filename)
+            img = image.load_img(img_path)
+            img = image.img_to_array(img)
+            x[count] = preprocess_input(img, mode='tf')
+            gt_file = os.path.join(cfg.data_dir,
+                                   cfg.train_label_dir_name,
+                                   img_filename[:-4] + '_gt.npy')
+            y[count] = np.load(gt_file)
+            count+=1
+            if count>=batch_size:
+                yield x,y
+                count=0
+        np.random.shuffle(f_list)
+
+
